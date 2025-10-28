252302c5afb47c28e61ece12242b544a5e858c21
human-321 <aidenpdonahue@gmail.com>
2025-10-28 16:02:56 -0400

    Create speak.py

---
+++ files/speak.py
@@ -0,0 +1,18 @@
+
+
+
+# # python3 -m piper.download_voices en_US-lessac-medium
+
+import wave
+from piper import PiperVoice
+
+
+# voice = PiperVoice.load("assets\en_US-lessac-medium.onnx" )
+
+
+def speak(words):
+    print(words)
+    # with wave.open("test.wav", "wb") as wav_file:
+    #     voice.synthesize_wav(words, wav_file)
+
+
